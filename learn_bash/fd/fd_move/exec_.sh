49da9d52f4af2dd043f5fa860a013d9cbcd1b194
embpgp <522943492@qq.com>
2016-06-23 21:52:21 +0800

    exec_fd.sh

---
+++ learn_bash/fd/fd_move/exec_.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+exec 3< /etc/passwd
+exec 0<&3
+cat
+
+
+exec 3>&-
+
+exec 3> /tmp/stdout
+exec 4> /tmp/stderr
+
+exec 1>&3-
+exec 2>&4-
+
+find /etc/ -name "passwd"
+
+exec 3>&-
+exec 4>&-
